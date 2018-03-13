da186bcfe3d732e18ffc944593f4f8bb2529c361
Paweł Krupa <paulfantom@gmail.com>
2018-03-13 14:28:31 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 ENGINE_PORT=9001
 
 if [ "$1" != "stop" ]; then
-  docker network create kafka-net
-  docker run -d --name zookeeper --network kafka-net zookeeper
-  docker run -d --name kafka --network kafka-net --env ZOOKEEPER_IP=zookeeper ches/kafka
-  docker run -d --name function-engine --env IC_QUEUE_HOSTS=kafka:9092 --network kafka-net -p ${ENGINE_PORT}:8080 sosoftware/function-engine:latest
-  docker run -d --name vcfjs --network kafka-net --restart on-failure:20 sosoftware/vcfjs:latest
+  docker network create vcf-net
+  docker run -d --network vcf-net --name zookeeper zookeeper
+  docker run -d --network vcf-net --name kafka --env ZOOKEEPER_IP=zookeeper ches/kafka
+  docker run -d --network vcf-net --name function-engine --env IC_QUEUE_HOSTS=kafka:9092 -p ${ENGINE_PORT}:8080 sosoftware/function-engine:latest
+  docker run -d --network vcf-net --name vcfjs --env KAFKA_HOST=kafka:9092 --restart on-failure:20 sosoftware/vcfjs:latest
   until docker exec vcfjs bash -c "cd ../function-engine-setup && yarn && yarn run setup:next"; do
     sleep 1
   done
@@ -15,7 +15,7 @@ if [ "$1" != "stop" ]; then
   echo -e "\e[32mRUNNING!\e[0m"
 else
   docker rm -f vcfjs function-engine kafka zookeeper
-  docker network rm kafka-net
+  docker network rm vcf-net
   echo
   echo -e "\e[31mDONE!\e[0m"
 fi
