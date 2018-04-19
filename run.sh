2fd8297f6e1c8c65da4869fb891eebd45caf1347
Paweł Krupa <paulfantom@gmail.com>
2018-04-19 13:37:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -43,17 +43,28 @@ if [ "$1" != "stop" ]; then
                   --restart on-failure:20 \
                   sosoftware/vcfjs:latest
     until docker exec "vcfjs$ID" bash -c "cd ../function-engine-setup && yarn && yarn run setup:next"; do sleep 1; done
-    PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' "hello$ID")
-    #curl -X PUT localhost:$PORT/repositories/A 
+    # PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' "hello$ID")
+    # curl -X PUT localhost:$PORT/repositories/A 
   done
   
-  docker exec redis bash -c 'redis-cli SADD http-A "vcfjs1" && redis-cli SADD http-B "vcfjs1" "vcfjs2" && redis-cli SADD http-C "vcfjs1" && redis-cli SADD file-A "vcfjs3" "vcfjs4" && redis-cli SADD file-B "vcfjs4" && redis-cli SET vcfjs1 "http" && redis-cli SET vcfjs2 "http" && redis-cli SET vcfjs3 "file" && redis-cli SET vcfjs4 "file" && redis-cli SET vcfjs5 "file"'
+  docker exec redis bash -c 'redis-cli SADD http-A vcfjs1 && \
+                             redis-cli SADD http-B vcfjs1 vcfjs2 && \
+                             redis-cli SADD http-C vcfjs1 && \
+                             redis-cli SADD file-A vcfjs3 vcfjs4 && \
+                             redis-cli SADD file-B vcfjs4 && \
+                             redis-cli SET vcfjs1 http && \
+                             redis-cli SET vcfjs2 http && \
+                             redis-cli SET vcfjs3 file && \
+                             redis-cli SET vcfjs4 file && \
+                             redis-cli SET vcfjs5 file'
 
   echo
+  # shellcheck disable=SC1117
   echo -e "\e[32mRUNNING!\e[0m"
 else
   docker rm -f vcfjs1 vcfjs2 vcfjs3 vcfjs4 vcfjs5 function-engine kafka zookeeper
   docker network rm vcf-net
   echo
+  # shellcheck disable=SC1117
   echo -e "\e[31mDONE!\e[0m"
 fi
